1e319fb8b8dab7346aa96d9ef4993fef7c0fc4df
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-01-30 18:26:47 -0500

    event handler 2

--- src/components/functionClick.component.js
+++ src/components/functionClick.component.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function FunctionClick() {
+
+    function clickHandler(){
+        alert("click")
+    }
+
     return (
         <div>
-            <button>Clickk</button>
+            <button onClick={clickHandler}>Clickk</button>
         </div>
     )
 }
