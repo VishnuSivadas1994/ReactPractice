e978ffde76d0acb24ecb43156f5676882bcf4173
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-01-30 19:07:46 -0500

    Added guides

--- src/App.js
+++ src/App.js
@@ -15,16 +15,22 @@ function App() {
       </Welcome>
       <Welcome name="Jett" alias="Jet" />
       <br />
+      <p>following will show method of setting up state and functions</p>
       <Counter />
+      <p>Following shows event handling in a function</p>
       <FunctionClick></FunctionClick>
+      <p>Following shows event handling in a class</p>
       <ClassClick></ClassClick>
       <br/>
       <br/>
+      <p>The following will describe four methods of event binding</p>
       <EventBind></EventBind>
       <br/>
       <br/>
       <br/>
       <br/>
+      <p>The following will pass a function from parent.js to child.js and in return 
+      get another prop from child and use it in the function in parent.js</p>
       <ParentComponent></ParentComponent>
     </div>
   );
